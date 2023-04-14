109b7f633a589e26fb911c343eb87ce464482c8d
slurmy <slurmy.slurm.slurm>
2023-04-14 11:34:35 +0200

    runs

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+print(000000, flush=True)
 # imports
 from dataloader import BreastCancerDatasetKaggle
 from torch.utils.data import DataLoader
@@ -6,24 +7,24 @@ import torchvision.transforms as T
 import torchmetrics
 import torch.nn as nn
 
-import tensorboard
+#import tensorboard
 
-
+print("oj", flush=True)
 # CUDA / CPU
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 # data
-data_path = "data/kaggle_data_processed.csv"
-image_base_path = "data/archive"
+data_path = "/d/hpc/home/ris002/joskogled/data/kaggle_data_processed.csv"
+image_base_path = "/d/hpc/home/ris002/joskogled/data/archive"
 
-# transformation = torch.nn.Sequential(T.RandomRotation(degrees=(-12, 12)))
+transformation = torch.nn.Sequential(T.RandomRotation(degrees=(-12, 12)))
 
-train_dataset = BreastCancerDatasetKaggle(data_path, image_base_path, split=[0, 10000], device=DEVICE)
-val_dataset = BreastCancerDatasetKaggle(data_path, image_base_path, split=[10000, -1], device=DEVICE)
+train_dataset = BreastCancerDatasetKaggle(data_path, image_base_path, split=[0, 1000], device=DEVICE)
+val_dataset = BreastCancerDatasetKaggle(data_path, image_base_path, split=[1000, 1300], device=DEVICE)
 
-train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=2,  shuffle=True)
+train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=8,  shuffle=True)
 
 # logging
 from model_meta import ModelMetrics
@@ -42,17 +43,17 @@ metrics = ModelMetrics(device=DEVICE, comment="moj_model_zanimiv")
 
 # Model Initialization
 from autoencoder import Autoencoder
-model = Autoencoder(base_channel_size=128, latent_dim=512, num_input_channels=1, width=512, height=512).to(DEVICE)
+model = Autoencoder(base_channel_size=64, latent_dim=864, num_input_channels=1, width=512, height=512).to(DEVICE)
  
 # Validation using MSE Loss function
 loss_f = nn.MSELoss( reduce=True, reduction='mean').to(DEVICE)
 # Using an Adam Optimizer with lr = 0.1
 optimizer = torch.optim.Adam(model.parameters(),
-                             lr = 1e-3,
+                             lr = 1e-5,
                              weight_decay = 1e-5)
 
 # training
-for epoch in range(10):
+for epoch in range(1):
     # train
     model.train()
     metrics.reset()
@@ -70,8 +71,8 @@ for epoch in range(10):
             loss.backward()
             optimizer.step()
 
-            print(f"Epoch: {epoch}, Batch: {i}, Loss: {loss.item()}")
+            print(f"Epoch: {epoch}, Batch: {i}, Loss: {loss.item()}",flush=True)
 
-            metrics.update(image, y_pred, loss.item(), show=True, batch=epoch, epoch=i)
+            metrics.update(image, y_pred, loss.item(), show=True, batch=epoch, epoch=epoch)
         metrics.reset()
         metrics.compute(show=True)
